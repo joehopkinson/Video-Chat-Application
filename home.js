fa7227e2c4eff70a68c4047284a8a126eb0af9b8
Joe Hopkinson <joe@hopkinson.co.uk>
2020-06-16 11:16:44 +0100

    Game Additions

--- home.js
+++ home.js
@@ -1,11 +1,21 @@
 var video = document.querySelector("#videoElement");
 
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-            })
-            .catch(function (error) {
-                console.log("Something went wrong!");
-            });
-    }
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.log("Something went wrong!");
+        });
+}
+
+function openPanel() {
+    document.getElementById("sidepanel").style.width = "250px";
+}
+  
+function closePanel() {
+    document.getElementById("sidepanel").style.width = "0";
+}
+
+// function togglePanel() {}
